bccd18082e820962795fab95860803312d316f84
jhonatanneves <nevesjn2@gmail.com>
2022-03-03 15:21:18 -0300

    att

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const res = require("express/lib/response");
+const cors = require("cors");
+
+app.use(cors());
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
